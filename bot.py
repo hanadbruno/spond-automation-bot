35400a14971276a6ab6b4d386df85cd280adfa43
hanadbruno <hanad1997@hotmail.no>
2025-01-02 11:16:14 +0100

    webmanager import

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
 
 
 # Get credentials from environment variables
